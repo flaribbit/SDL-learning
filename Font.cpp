2922c13dd58f81f47ffd9bf3af7ceada6404c62c
flaribbit <1149761294@qq.com>
2020-02-11 11:59:46 +0800

    👌 upfate: fontcache优化

--- Font.cpp
+++ Font.cpp
@@ -10,42 +10,55 @@ Font::~Font()
     ReleaseAll();
 }
 
+uint32_t GetUTF8Char(unsigned char *&p,char *c){
+    uint32_t code;
+    while(*p>=128&&*p<192){
+        p++;
+    }
+    if(*p>=240){
+        code=*(uint32_t*)p;
+        if(c){
+            c[0]=p[0];
+            c[1]=p[1];
+            c[2]=p[2];
+            c[2]=p[3];
+            c[4]=0;
+        }
+        p+=4;
+    }else if(*p>=224){
+        code=p[2]<<16|p[1]<<8|*p;
+        if(c){
+            c[0]=p[0];
+            c[1]=p[1];
+            c[2]=p[2];
+            c[3]=0;
+        }
+        p+=3;
+    }else if(*p>=192){
+        code=*(uint16_t*)p;
+        if(c){
+            c[0]=p[0];
+            c[1]=p[1];
+            c[2]=0;
+        }
+        p+=2;
+    }else{
+        code=*p;
+        if(c){
+            c[0]=p[0];
+            c[1]=0;
+        }
+        p+=1;
+    }
+    return code;
+}
+
 void Font::CreateCache(const char *string){
     unsigned char *p=(unsigned char *)string;
     SDL_Color color={255,255,255,255};
     while(*p){
-        if(*p>=128&&*p<192){
-            p++;
-            continue;
-        }
-        uint32_t code;
         char c[5]={0};
-        if(*p>=240){
-            code=*(uint32_t*)p;
-            for(int i=0;i<4;i++){
-                c[i]=p[i];
-            }
-            c[4]=0;
-            p+=4;
-        }else if(*p>=224){
-            code=p[2]<<16|p[1]<<8|*p;
-            for(int i=0;i<3;i++){
-                c[i]=p[i];
-            }
-            c[3]=0;
-            p+=3;
-        }else if(*p>=192){
-            code=*(uint16_t*)p;
-            c[0]=p[0];
-            c[1]=p[1];
-            c[2]=0;
-            p+=2;
-        }else{
-            code=*p;
-            c[0]=p[0];
-            c[1]=0;
-            p+=1;
-        }
+        uint32_t code=GetUTF8Char(p,c);
         auto item=this->Cache.find(code);
         if(item==this->Cache.end()){
             this->Cache[code]=new Image(TTF_RenderUTF8_Blended(this->font,c,color));
@@ -65,24 +78,7 @@ void Font::Print(const char *string,int x,int y){
     CreateCache(string);
     unsigned char *p=(unsigned char *)string;
     while(*p){
-        if(*p>=128&&*p<192){
-            p++;
-            continue;
-        }
-        uint32_t code;
-        if(*p>=240){
-            code=*(uint32_t*)p;
-            p+=4;
-        }else if(*p>=224){
-            code=p[2]<<16|p[1]<<8|*p;
-            p+=3;
-        }else if(*p>=192){
-            code=*(uint16_t*)p;
-            p+=2;
-        }else{
-            code=*p;
-            p+=1;
-        }
+        uint32_t code=GetUTF8Char(p,0);
         Image *t=this->Cache[code];
         t->Draw(x,y);
         x+=t->GetWidth();
@@ -92,23 +88,11 @@ void Font::Print(const char *string,int x,int y){
 void Font::Release(const char *c){
     unsigned char *p=(unsigned char *)c;
     while(*p){
-        if(*p>=128&&*p<192){
-            p++;
-            continue;
-        }
-        uint32_t code;
-        if(*p>=240){
-            code=*(uint32_t*)p;
-            p+=4;
-        }else if(*p>=224){
-            code=p[2]<<16|p[1]<<8|*p;
-            p+=3;
-        }else if(*p>=192){
-            code=*(uint16_t*)p;
-            p+=2;
-        }else{
-            code=*p;
-            p+=1;
+        uint32_t code=GetUTF8Char(p,0);
+        auto item=this->Cache.find(code);
+        if(item!=this->Cache.end()){
+            delete item->second;
+            this->Cache.erase(item->first);
         }
     }
 }
