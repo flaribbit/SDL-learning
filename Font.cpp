e8715c63c41d70cec71d06a528f4bfe1e79e5c8e
flaribbit <1149761294@qq.com>
2020-02-16 21:46:13 +0800

    🍻 update: 换了个字体逻辑

--- Font.cpp
+++ Font.cpp
@@ -11,9 +11,8 @@ Font::~Font()
 }
 
 //Converter from: https://github.com/RoelofBerg/Utf8Ucs2Converter/blob/master/Utf8Ucs2Converter.cpp
-unsigned char ConvertChar(const char* utf8char, wchar_t* ucs2char)
+unsigned char ConvertChar(unsigned char* p, wchar_t* ucs2char)
 {
-    const unsigned char *p=(const unsigned char*)utf8char;
     *ucs2char = L'?';
     if(0x80>p[0]){
         *ucs2char=(wchar_t)p[0];//1字节字符
@@ -39,59 +38,17 @@ unsigned char ConvertChar(const char* utf8char, wchar_t* ucs2char)
     }
 }
 
-uint32_t GetUTF8Char(unsigned char *&p,char *c){
-    uint32_t code;
-    while(*p>=128&&*p<192){
-        p++;
-    }
-    if(*p>=240){
-        code=*(uint32_t*)p;
-        if(c){
-            c[0]=p[0];
-            c[1]=p[1];
-            c[2]=p[2];
-            c[2]=p[3];
-            c[4]=0;
-        }
-        p+=4;
-    }else if(*p>=224){
-        code=p[2]<<16|p[1]<<8|*p;
-        if(c){
-            c[0]=p[0];
-            c[1]=p[1];
-            c[2]=p[2];
-            c[3]=0;
-        }
-        p+=3;
-    }else if(*p>=192){
-        code=*(uint16_t*)p;
-        if(c){
-            c[0]=p[0];
-            c[1]=p[1];
-            c[2]=0;
-        }
-        p+=2;
-    }else{
-        code=*p;
-        if(c){
-            c[0]=p[0];
-            c[1]=0;
-        }
-        p+=1;
-    }
-    return code;
-}
-
 void Font::CreateCache(const char *string){
     unsigned char *p=(unsigned char *)string;
     SDL_Color color={255,255,255,255};
+    wchar_t code[2]={0,0};
     while(*p){
-        char c[5]={0};
-        uint32_t code=GetUTF8Char(p,c);
-        auto item=this->Cache.find(code);
+        unsigned char codelen=ConvertChar(p,code);
+        auto item=this->Cache.find(*code);
         if(item==this->Cache.end()){
-            this->Cache[code]=new Image(TTF_RenderUTF8_Blended(this->font,c,color));
+            this->Cache[*code]=new Image(TTF_RenderUNICODE_Blended(this->font,(const uint16_t*)code,color));
         }
+        p+=codelen?codelen:1;
     }
 }
 
@@ -106,23 +63,27 @@ void Font::NewFont(const char *ttf_file,int size){
 void Font::Print(const char *string,int x,int y){
     CreateCache(string);
     unsigned char *p=(unsigned char *)string;
+    wchar_t code[2]={0,0};
     while(*p){
-        uint32_t code=GetUTF8Char(p,0);
-        Image *t=this->Cache[code];
+        unsigned char codelen=ConvertChar(p,code);
+        Image *t=this->Cache[*code];
         t->Draw(x,y);
         x+=t->GetWidth();
+        p+=codelen?codelen:1;
     }
 }
 
 void Font::Release(const char *c){
     unsigned char *p=(unsigned char *)c;
+    wchar_t code[2]={0,0};
     while(*p){
-        uint32_t code=GetUTF8Char(p,0);
-        auto item=this->Cache.find(code);
+        unsigned char codelen=ConvertChar(p,code);
+        auto item=this->Cache.find(*code);
         if(item!=this->Cache.end()){
             delete item->second;
             this->Cache.erase(item->first);
         }
+        p+=codelen?codelen:1;
     }
 }
 
